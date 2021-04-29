790e8f089b7482b92e156b937eb41508406295f8
Michelle Peters <mpeters90@academic.rrc.ca>
2021-04-28 19:57:37 -0500

    Create file for random playlist search.

---
+++ app/src/main/java/com/example/genrefy/RandomSearch.java
@@ -0,0 +1,14 @@
+package com.example.genrefy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RandomSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_random_search);
+    }
+}
